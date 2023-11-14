98a7724f154043b49f7d316318bc829821c7ff85
Vinicios21 <vinicios_divino@estudante.sesisenai.org.br>
2023-11-13 21:49:20 -0300

    second commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,13 @@
 class ApplicationController < ActionController::Base
     before_action :set_locale
     def set_locale
-      if params[:locale]
-        cookies[:locale] = params[:locale]
-    end 
-    if cookies[locale]
-        if Il8n.locale != cookies[:locale]
-            Il8n.locale = cookies[:locale]
+        if params[:locale]
+            cookies[:locale] = params[:locale]
         end 
-    end       
+        if cookies[locale]
+            if Il8n.locale != cookies[:locale]
+                Il8n.locale = cookies[:locale]
+            end 
+        end       
+    end
 end
-
-     
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,6 @@
 class WelcomeController < ApplicationController
   def index
+    cookies[:teste_C] = "Vinicios"
+    session[:teste_S] = "Moura"
   end
 end
